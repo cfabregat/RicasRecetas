2eae110d6e3e5ff7997498499faaed0074877bc0
Cristian Mauro FABREGAT <cfabregat@webrecursos.com.ar>
2023-05-18 21:33:57 -0300

    Prueba

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder, public alertController: AlertController) {
+    
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirm-password': new FormControl("", Validators.required)
+    })
+   }
+   ngOnInit() {
+  }
+
+  async guardar(){
+    
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Atención!!',
+        message: 'Se deben ingresar todos los datos',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+    }
+    // Este reset sirve para resetear todos los campos del formulario
+    else{
+      this.formularioRegistro.reset();
+    }
+
+    var usuario = {
+      nombre: f.nombre,
+      password: f.password
+    }
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+  }
+}
+
+
