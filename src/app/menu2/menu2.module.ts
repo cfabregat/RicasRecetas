4db0c7f17d97871e6c5555df582e22a71572a3ec
Jorge Leiba <jorgeleiba@gmail.com>
2023-05-27 20:51:39 -0300

    Agregando slides

--- src/app/menu2/menu2.module.ts
+++ src/app/menu2/menu2.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { Menu2PageRoutingModule } from './menu2-routing.module';
-
 import { Menu2Page } from './menu2.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   imports: [
@@ -15,6 +15,15 @@ import { Menu2Page } from './menu2.page';
     IonicModule,
     Menu2PageRoutingModule
   ],
-  declarations: [Menu2Page]
+  declarations: [Menu2Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Menu2PageModule {}
+
+
+/*
+1-Hay que importar: import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+2-En @NgModule declarar el schema: schemas: [CUSTOM_ELEMENTS_SCHEMA],
+3-En app.component.ts importar: import { register } from 'swiper/element/bundle'
+4-En app.component.ts declarar: register()
+*/
