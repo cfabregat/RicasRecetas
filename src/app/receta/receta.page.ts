92afcc522412186d3921059e444f9353d73aba9d
SofiMaida <soofi.maidana@gmail.com>
2023-06-01 21:53:07 -0300

    Queda por resolver la url del buscador por ID

--- src/app/receta/receta.page.ts
+++ src/app/receta/receta.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-receta',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecetaPage implements OnInit {
 
-  constructor() { }
+ 
+  recetaId?: string | null;
+  chracter!: any;
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+    
+    }
 
   ngOnInit() {
+    this.recetaId = this.activatedRoute.snapshot.paramMap.get("idMeal")
+    this.http
+      .get("www.themealdb.com/api/json/v1/1/lookup.php?i" + this.recetaId)
+      .subscribe(res => this.chracter = res)  }
+
   }
 
-}
+
