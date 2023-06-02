7a0952d1109a3753644cc72c22302891cf4d1577
Brisa Sardón <sardonbrisa21@gmail.com>
2023-06-02 16:41:45 -0300

    Agregando items y con sus rutas

--- src/app/component/menu-desplegable/menu-desplegable.component.ts
+++ src/app/component/menu-desplegable/menu-desplegable.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterEvent, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu-desplegable',
   templateUrl: './menu-desplegable.component.html',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuDesplegableComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {}
-
+  async cerrar_session(){
+    this.router.navigate(['/login']);
+    // const alert = await this.alertController.create({
+    //   header: '***Falta***',
+    //   message: 'Escribir el codigo para cerrar eliminar el localstore(login, autologin)',
+    //   buttons: ['Aceptar']
+    // });
+    // await alert.present();
+  }
 }
