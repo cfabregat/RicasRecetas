d7b933d199484d755029462a91f069b5bb2531ed
Brisa Sardón <sardonbrisa21@gmail.com>
2023-06-06 10:31:24 -0300

    conectando en el menu a paginas

--- src/app/component/menu-desplegable/menu-desplegable.component.ts
+++ src/app/component/menu-desplegable/menu-desplegable.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterEvent, RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-menu-desplegable',
   templateUrl: './menu-desplegable.component.html',
   styleUrls: ['./menu-desplegable.component.scss'],
 })
 export class MenuDesplegableComponent  implements OnInit {
+  
+  /* menuOpts: any;  */
 
   constructor(private router:Router) { }
 
   ngOnInit() {}
+   /*  this.menuOpts = [
+      {
+        "url": "./menu2/menu2.module",
+        "iconName":"library-outline",
+        "optionName":"Recetas"
+      },
+      {
+        "url": "./favoritos/favoritos.module",
+        "iconName":"book-outline",
+        "optionName":"Mis Preparaciones"
+      }
+    ]; */
+  
+
   async cerrar_session(){
     this.router.navigate(['/login']);
     // const alert = await this.alertController.create({
@@ -20,4 +37,5 @@ export class MenuDesplegableComponent  implements OnInit {
     // });
     // await alert.present();
   }
+
 }
